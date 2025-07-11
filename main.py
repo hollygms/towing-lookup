6b96fc81c3a549d21d641fe0e49d4202a7dcfbb6
hollygms <hollygms@gmail.com>
2025-07-10 21:58:58 -0700

    Putting everything in main in a if name == main check

--- main.py
+++ main.py
@@ -2,11 +2,13 @@ from config import dt, CSV_FILE, FORD_URL, TOWING_COLUMN, VIN_COLUMN, SOURCE_COL
 from ford import get_ford_towing
 from validate_vin import check_nhtsa_api
 
-check_nhtsa_api()
-vin_towing = get_ford_towing()
 
-for key, value in vin_towing.items():
-    dt.loc[dt[VIN_COLUMN] == key, TOWING_COLUMN] = value
-    dt.loc[dt[VIN_COLUMN] == key, SOURCE_COLUMN] = FORD_URL
+if __name__ == '__main__':
+    check_nhtsa_api()
+    vin_towing = get_ford_towing()
 
-dt.to_csv(CSV_FILE, index=False)
+    for key, value in vin_towing.items():
+        dt.loc[dt[VIN_COLUMN] == key, TOWING_COLUMN] = value
+        dt.loc[dt[VIN_COLUMN] == key, SOURCE_COLUMN] = FORD_URL
+
+    dt.to_csv(CSV_FILE, index=False)
