b3efacb78a0edef4cbb03693613b6066f0dd1ba2
hollygms <hollygms@gmail.com>
2025-08-10 21:32:20 -0700

    Updating validate vin to be a bit cleaner

--- validate_vin.py
+++ validate_vin.py
@@ -1,26 +1,33 @@
-from config import dt, NHTSA_URL, CSV_FILE, VIN_COLUMN, ENGINE_COLUMN, TOWING_COLUMN
+import pandas as pd
+from config import NHTSA_URL, VIN_COLUMN, ENGINE_COLUMN, TOWING_COLUMN, dt
 import requests
 
-def check_nhtsa_api():
+def get_validated_dataframe(df: pd.DataFrame) -> pd.DataFrame:
     """Loops through the database's VIN column and runs each vin through the NHTSA API.
     Updates Towing and Engine columns to VIN ERROR if a 400 error is received from API.
     Otherwise, just updates the engine and fuel type"""
-    for i, vin in enumerate(dt[VIN_COLUMN], 1):
-        if type(vin) == str and vin != VIN_COLUMN:  # skipping empty vin columns or repeat header names
-            response = requests.get(f'{NHTSA_URL}/{vin}?format=json').json()
-            validation = response['Results'][0]['ErrorCode']
-            engine = response['Results'][0]['DisplacementL']
-            fuel = response['Results'][0]['FuelTypePrimary']
+    #drops all blank vin numbers
+    dt.dropna(subset=[VIN_COLUMN], inplace=True) 
 
-            # checks for error code
-            if validation == '1,400':
-                dt.at[i-1, TOWING_COLUMN] = 'VIN ERROR'
-                dt.at[i-1, ENGINE_COLUMN] = 'VIN ERROR'
-            else:
-                dt.at[i-1, ENGINE_COLUMN] = f'{engine}L/{fuel}'
+    #selects only the vins that have nothing in the engine column
+    missing_engine_dt = dt[dt[ENGINE_COLUMN].isna()]
+    
+    for i, vin in enumerate(missing_engine_dt[VIN_COLUMN]):
+        response = requests.get(f'{NHTSA_URL}/{vin}?format=json').json()
+        validation = response['Results'][0]['ErrorCode']
+        engine = response['Results'][0]['DisplacementL']
+        fuel = response['Results'][0]['FuelTypePrimary']
+ 
+        # checks for vin error code
+        if validation == '1,400':
+            dt.at[i, TOWING_COLUMN] = 'VIN ERROR'
+            dt.at[i, ENGINE_COLUMN] = 'VIN ERROR'
+        else:
+            dt.at[i, ENGINE_COLUMN] = f'{engine}L/{fuel}'
 
-        if i % 50 == 0:  # quick progress check
+        if i+1 % 50 == 0:  # quick progress check
             print(f'Processed {i} VINs')
 
-    dt.to_csv(CSV_FILE, index=False)
+    return dt
 
+#get_validated_dataframe(df=dt)
